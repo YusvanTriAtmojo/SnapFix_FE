16548736fe1d4c7f131c4e6eea408c183e2e5c85
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2026-07-30 18:48:02 +0700

    Snackbar state eror untuk profile klien

--- lib/presentation/klien/profile/klien_profile_screen.dart
+++ lib/presentation/klien/profile/klien_profile_screen.dart
@@ -42,15 +42,37 @@ class _KlienProfileScreenState extends State<KlienProfileScreen> {
             ),
           ),
           child: SafeArea(
-            child: BlocBuilder<KlienBloc, KlienState>(
+            child: BlocConsumer<KlienBloc, KlienState>(
+              listener: (context, state) {
+                if (state is KlienFailure) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(
+                        state.error,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      duration: const Duration(seconds: 2),
+                      backgroundColor: Colors.white,
+                      behavior: SnackBarBehavior.floating,
+                      margin: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 10,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      elevation: 6,
+                    ),
+                  );
+                }
+              },
               builder: (context, state) {
                 if (state is KlienLoading) {
                   return const Center(
                     child: CircularProgressIndicator(color: Color(0xFF002F87)),
-                  );
-                } else if (state is KlienFailure) {
-                  return Center(
-                    child: Text("Gagal memuat data: ${state.error}"),
                   );
                 } else if (state is KlienLoaded) {
                   final DataUser klien = state.klien;
@@ -181,7 +203,7 @@ class _KlienProfileScreenState extends State<KlienProfileScreen> {
                     ],
                   );
                 } else {
-                  return const Center(child: Text("Belum ada data Teknisi"));
+                  return const Center(child: Text("Belum ada data Klien"));
                 }
               },
             ),
